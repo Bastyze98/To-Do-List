bab1e2d78830b4f91443d6e6886ad1e1aeeeb58c
Chirvai Sebastian <chirvai.sebi@gmail.com>
2024-02-18 23:16:31 +0200

    created a row state for the rows and displayed them dinamically

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,6 +1,5 @@
-import buttonStyle from "./modal.module.css";
-import styles from "./table.module.css";
 import { useEffect, useState } from "react";
+import styles from "./table.module.css";
 import Modal from "./Modal";
 
 import { BsFillPencilFill, BsFillTrashFill } from "react-icons/bs";
@@ -11,6 +10,12 @@ function Table() {
   }, []);
 
   const [modalOpen, setModalOpen] = useState(false);
+
+  const rows = [
+    { page: "Page 1", description: "This is the first page", status: "live" },
+    { page: "Page 2", description: "This is the second page", status: "draft" },
+    { page: "Page 3", description: "This is the third page", status: "error" },
+  ];
 
   return (
     <div className={styles["table-wrapper"]}>
@@ -24,59 +29,41 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Home</td>
-            <td>This is the main page</td>
-            <td>
-              <span className={`${styles.label} ${styles.labelLive}`}>
-                Live
-              </span>
-            </td>
-            <td>
-              <span className={styles["actions"]}>
-                <BsFillTrashFill className={styles["delete-btn"]} />
-                <BsFillPencilFill />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>Page 2</td>
-            <td>This is the second page</td>
-            <td>
-              <span className={`${styles.label} ${styles.labelDraft}`}>
-                Draft
-              </span>
-            </td>
-            <td>
-              <span className={styles["actions"]}>
-                <BsFillTrashFill className={styles["delete-btn"]} />
-                <BsFillPencilFill />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>Page 3</td>
-            <td>This is the third page</td>
-            <td>
-              <span className={`${styles.label} ${styles.labelError}`}>
-                Draft
-              </span>
-            </td>
-            <td>
-              <span className={styles["actions"]}>
-                <BsFillTrashFill className={styles["delete-btn"]} />
-                <BsFillPencilFill />
-              </span>
-            </td>
-          </tr>
+          {rows.map((row, idx) => {
+            const statusText =
+              row.status.charAt(0).toUpperCase() + row.status.slice(1);
+            return (
+              <tr key={idx}>
+                <td>{row.page}</td>
+                <td className={styles["expand"]}>{row.description}</td>
+                <td>
+                  <span
+                    className={`${styles["label"]} ${
+                      styles[`label-${row.status}`]
+                    }`}
+                  >
+                    {statusText}
+                  </span>
+                </td>
+                <td>
+                  <span className={styles["actions"]}>
+                    <BsFillTrashFill className={styles["delete-btn"]} />
+                    <BsFillPencilFill />
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
-      <button
-        className={buttonStyle["btn-form"]}
-        onClick={() => setModalOpen(true)}
-      >
-        Add
-      </button>
+      <div className={styles["btn-container"]}>
+        <button
+          className={styles["btn-form"]}
+          onClick={() => setModalOpen(true)}
+        >
+          Add
+        </button>
+      </div>
       {modalOpen && (
         <Modal
           closeModal={() => {
